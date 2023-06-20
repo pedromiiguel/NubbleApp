36efece741cc6b22e9ba2ae7b65b5fb80013a554
Pedro Miguel <pedromrap@gmail.com>
2023-06-20 20:55:55 -0300

    feat(Box): add TouchableOpacityBox component

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,4 +1,34 @@
-import {createBox} from '@shopify/restyle';
+import {
+  createBox,
+  createRestyleComponent,
+  backgroundColor,
+  BackgroundColorProps,
+  spacing,
+  SpacingProps,
+  layout,
+  LayoutProps,
+  border,
+  BorderProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
 import {Theme} from '../../theme/theme';
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
 
 export const Box = createBox<Theme>();
+export type BoxProps = React.ComponentProps<typeof Box>;
+
+export type TouchableOpacityBoxProps = BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  LayoutProps<Theme> &
+  BorderProps<Theme> &
+  SpacingShorthandProps<Theme> &
+  TouchableOpacityProps;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [backgroundColor, spacing, layout, border, spacingShorthand],
+  TouchableOpacity,
+);
