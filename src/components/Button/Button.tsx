0e1c197b3a325a43b51c35a186a491c525596833
Pedro Miguel <pedromrap@gmail.com>
2023-06-20 21:08:26 -0300

    feat(Button): add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {ActivityIndicator} from 'react-native';
+import {Text} from '../Text/Text';
+import {TouchableOpacityBox, TouchableOpacityBoxProps} from '../Box/Box';
+import {buttonPresets} from './buttonPreset';
+
+export type ButtonPreset = 'primary' | 'outline';
+
+interface ButtonProps extends TouchableOpacityBoxProps {
+  title: string;
+  loading?: boolean;
+  preset?: ButtonPreset;
+  disabled?: boolean;
+}
+
+export function Button({
+  title,
+  loading = false,
+  preset = 'primary',
+  disabled = false,
+  ...touchableOpacityBoxProps
+}: ButtonProps) {
+  const buttonPreset = buttonPresets[preset][disabled ? 'disabled' : 'default'];
+
+  return (
+    <TouchableOpacityBox
+      disabled={disabled || loading}
+      paddingHorizontal="s20"
+      height={50}
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="s16"
+      {...buttonPreset.container}
+      {...touchableOpacityBoxProps}>
+      {loading ? (
+        <ActivityIndicator color={buttonPreset.content} />
+      ) : (
+        <Text preset="paragraphMedium" bold color={buttonPreset.content}>
+          {title}
+        </Text>
+      )}
+    </TouchableOpacityBox>
+  );
+}
