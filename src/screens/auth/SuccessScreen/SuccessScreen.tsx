c454170b3e002da14258b41e6f27b5ec803b468e
Pedro Miguel <pedromrap@gmail.com>
2023-07-08 20:22:09 -0300

    feat: add success screen

---
+++ src/screens/auth/SuccessScreen/SuccessScreen.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Screen} from '../../../components/Screen/Screen';
+import {Icon} from '../../../components/Icon/Icon';
+import {Text} from '../../../components/Text/Text';
+import {Button} from '../../../components/Button/Button';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../../routes/routes';
+
+type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SuccessScreen'>;
+
+export function SuccessScreen({route}: ScreenProps) {
+  const {title, description, icon} = route.params;
+
+  function goBackToBegin() {}
+
+  return (
+    <Screen>
+      <Icon {...icon} />
+      <Text preset="headingLarge" mt="s24">
+        {title}
+      </Text>
+      <Text preset="paragraphLarge" mt="s16">
+        {description}
+      </Text>
+
+      <Button mt="s40" onPress={goBackToBegin} title="Voltar ao início" />
+    </Screen>
+  );
+}
