3d74477fdf0f300da54175263237752b312e3461
Pedro Miguel <pedromrap@gmail.com>
2023-07-09 20:19:39 -0300

    feat: add FormTextInput component

---
+++ src/components/Form/FormTextInput.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Controller, UseControllerProps, FieldValues} from 'react-hook-form';
+import {TextInput, TextInputProps} from '../TextInput/TextInput';
+
+interface FormTextInputProps extends TextInputProps {
+  control: any;
+  name: string;
+  rules: any;
+}
+
+export function FormTextInput<FormType extends FieldValues>({
+  control,
+  name,
+  rules,
+  ...textInputProps
+}: FormTextInputProps & UseControllerProps<FormType>) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({field, fieldState}) => (
+        <TextInput
+          value={field.value}
+          onChangeText={field.onChange}
+          errorMessage={fieldState.error?.message}
+          {...textInputProps}
+        />
+      )}
+    />
+  );
+}
