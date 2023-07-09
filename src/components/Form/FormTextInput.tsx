8eed63f06e9539c396cf6b443786e7abe09cec3f
Pedro Miguel <pedromrap@gmail.com>
2023-07-09 20:31:56 -0300

    chore(FormTextInput): remove unused interface

--- src/components/Form/FormTextInput.tsx
+++ src/components/Form/FormTextInput.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 import {Controller, UseControllerProps, FieldValues} from 'react-hook-form';
 import {TextInput, TextInputProps} from '../TextInput/TextInput';
 
-interface FormTextInputProps extends TextInputProps {
-  control: any;
-  name: string;
-  rules: any;
-}
-
 export function FormTextInput<FormType extends FieldValues>({
   control,
   name,
   rules,
   ...textInputProps
-}: FormTextInputProps & UseControllerProps<FormType>) {
+}: TextInputProps & UseControllerProps<FormType>) {
   return (
     <Controller
       control={control}
