cf7742a8fd5f247311d4accda55c3a653ce9716b
Pedro Miguel <pedromrap@gmail.com>
2023-07-17 12:06:58 -0300

    feat: add favorite screen

---
+++ src/screens/app/FavoriteScreen/FavoriteScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Text} from '@components';
+import {AppTabScreenProps} from '@routes';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function FavoriteScreen(props: AppTabScreenProps<'FavoriteScreen'>) {
+  return <Text preset="headingSmall">Favorite Screen</Text>;
+}
