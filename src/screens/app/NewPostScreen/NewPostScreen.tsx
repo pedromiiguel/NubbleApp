49c2d06a7de7f7aa087317b446c700b29cfa9b9e
Pedro Miguel <pedromrap@gmail.com>
2023-07-17 12:07:35 -0300

    feat: add new post screen

---
+++ src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Text} from '@components';
+import {AppTabScreenProps} from '@routes';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function NewPostScreen(props: AppTabScreenProps<'NewPostScreen'>) {
+  return <Text preset="headingSmall">New Post Screen</Text>;
+}
