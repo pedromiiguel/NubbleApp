10b2cba15c1627b44df830b97a9aa31411af8452
Pedro Miguel <pedromrap@gmail.com>
2023-07-19 17:38:50 -0300

    feat: post domain integration with the API

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,9 +1,18 @@
-import {postListMock} from './postListMock';
-import {Post} from './types';
+import {PageAPI} from '@api';
 
-async function getList(): Promise<Post[]> {
-  //TODO: simular um delay na API
-  return postListMock;
+import {PostAPI} from './postType';
+
+async function getList(): Promise<PageAPI<PostAPI>> {
+  const response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer Mg.sxEEtdnVQIIvFI5EkPG1RM5tJ71zmtLwZNzVTaY4IhE6Xi8vaI4iD48-4Hx4',
+    },
+  });
+
+  let data: PageAPI<PostAPI> = await response.json();
+  return data;
 }
 
 export const postApi = {
