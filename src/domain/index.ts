1af40061258b6b1c939be19e047f22bc496561ca
Pedro Miguel <pedromrap@gmail.com>
2023-08-15 17:08:05 -0300

    feat: add useUser hook

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,5 +2,5 @@ export * from './Post/postType';
 export * from './Post/postService';
 export * from './Post/useCases/usePostList';
 
-export * from './hooks/usePaginatedList';
 export * from './PostComment';
+export * from './Auth';
