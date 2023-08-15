6a80f05c89607f3fc8befb426640e26ff0346c88
Pedro Miguel <pedromrap@gmail.com>
2023-08-15 10:00:43 -0300

    chore: exports

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -5,6 +5,8 @@ export * from './auth/SuccessScreen/SuccessScreen';
 
 export * from './app/HomeScreen/HomeScreen';
 export * from './app/SettingsScreen/SettingsScreen';
+export * from './app/PostCommentScreen/PostCommentScreen';
+
 export * from './app/FavoriteScreen/FavoriteScreen';
 export * from './app/MyProfileScreen/MyProfileScreen';
 export * from './app/NewPostScreen/NewPostScreen';
