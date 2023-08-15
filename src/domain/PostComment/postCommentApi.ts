516364e4b481d3b5a2cde71e1df65f7055b0b2c5
Pedro Miguel <pedromrap@gmail.com>
2023-08-15 17:09:14 -0300

    chore: add remove post comment service

--- src/domain/PostComment/postCommentApi.ts
+++ src/domain/PostComment/postCommentApi.ts
@@ -2,19 +2,18 @@ import {PageAPI, PageParams, api} from '@api';
 
 import {PostCommentAPI} from './postCommentTypes';
 
+const PATH = '/user/post_comment';
+
 async function getList(
   post_id: number,
   pageParams?: PageParams,
 ): Promise<PageAPI<PostCommentAPI>> {
-  const response = await api.get<PageAPI<PostCommentAPI>>(
-    '/user/post_comment',
-    {
-      params: {
-        post_id,
-        pageParams,
-      },
+  const response = await api.get<PageAPI<PostCommentAPI>>(PATH, {
+    params: {
+      post_id,
+      pageParams,
     },
-  );
+  });
   return response.data;
 }
 
@@ -22,7 +21,7 @@ async function create(
   post_id: number,
   message: string,
 ): Promise<PostCommentAPI> {
-  const response = await api.post<PostCommentAPI>('/user/post_comment', {
+  const response = await api.post<PostCommentAPI>(PATH, {
     post_id,
     message,
   });
@@ -30,7 +29,16 @@ async function create(
   return response.data;
 }
 
+async function remove(postCommentId: number): Promise<{message: string}> {
+  const response = await api.delete<{message: string}>(
+    `${PATH}/${postCommentId}`,
+  );
+
+  return response.data;
+}
+
 export const postCommentApi = {
   getList,
   create,
+  remove,
 };
--- src/domain/PostComment/postCommentService.ts
+++ src/domain/PostComment/postCommentService.ts
@@ -26,7 +26,38 @@ async function create(post_id: number, message: string): Promise<PostComment> {
   return postCommentAdapter.toPostComment(postCommentAPI);
 }
 
+async function remove(postCommentId: number): Promise<string> {
+  const {message} = await postCommentApi.remove(postCommentId);
+
+  return message;
+}
+
+/**
+ * @description user can delete the comment if it is the post author or comment author
+ * @param userId the current session user id
+ * @param postComment comment to be deleted
+ * @param postAuthorId the id of the post author
+ */
+
+function isAllowToDelete(
+  userId: number,
+  postComment: PostComment,
+  postAuthorId: number,
+): boolean {
+  if (postComment.author.id === userId) {
+    return true;
+  }
+
+  if (postAuthorId === userId) {
+    return true;
+  }
+
+  return false;
+}
+
 export const postCommentService = {
   getList,
   create,
+  remove,
+  isAllowToDelete,
 };
