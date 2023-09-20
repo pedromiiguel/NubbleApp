dbff6a9eb77d40006c5d82eb3c45e638f104e80d
Pedro Miguel <pedromrap@gmail.com>
2023-09-20 11:30:55 -0300

    feat: add profile screen

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -6,6 +6,7 @@ export * from './auth/SuccessScreen/SuccessScreen';
 export * from './app/HomeScreen/HomeScreen';
 export * from './app/SettingsScreen/SettingsScreen';
 export * from './app/PostCommentScreen/PostCommentScreen';
+export * from './app/ProfileScreen/ProfileScreen';
 
 export * from './app/FavoriteScreen/FavoriteScreen';
 export * from './app/MyProfileScreen/MyProfileScreen';
