1a4f26be0c32ba04da758af28032e9c49c13d996
Pedro Miguel <pedromrap@gmail.com>
2023-09-21 16:35:56 -0300

    chore: remove mock headers authorization

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,8 +2,4 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
-  headers: {
-    Authorization:
-      'Bearer MQ.wuahDLFI0FVLrqKLq1UJ3pIqFB2yApVpfds20a8p3Sg8r5NZUfTfLQ8wPq2W',
-  },
 });
