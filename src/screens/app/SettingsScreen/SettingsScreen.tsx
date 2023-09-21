53322325d9c14ae6e2ece25978e25ee032fbe800
Pedro Miguel <pedromrap@gmail.com>
2023-09-21 16:52:42 -0300

    feat(SettingsScreen): add button to sign out

--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
+import {useAuthSignOut} from '@domain';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
-import {Screen, Text} from '@components';
+import {Button, Screen} from '@components';
 import {AppStackParamList} from '@routes';
 
 type ScreenProps = NativeStackScreenProps<AppStackParamList, 'SettingsScreen'>;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function SettingsScreen(props: ScreenProps) {
+  const {isLoading, signOut} = useAuthSignOut();
+
   return (
-    <Screen canGoBack>
-      <Text preset="paragraphLarge">Settings screen</Text>
+    <Screen canGoBack title="Configurações">
+      <Button loading={isLoading} title="Sair da conta" onPress={signOut} />
     </Screen>
   );
 }
