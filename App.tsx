5e40833285befeea6e87a303eb584a5aa691fdd0
Pedro Miguel <pedromrap@gmail.com>
2023-09-25 12:12:43 -0300

    feat: add useAuthCredentials

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import {
+  AuthCredentialsProvider,
+  MMKVStorage,
+  initializeStorage,
+} from '@services';
 import {ThemeProvider} from '@shopify/restyle';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -9,18 +14,22 @@ import {Toast} from '@components';
 import {Router} from './src/routes/routes';
 import {theme} from './src/theme/theme';
 
+initializeStorage(MMKVStorage);
+
 const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <ThemeProvider theme={theme}>
-          <Router />
-          <Toast />
-        </ThemeProvider>
-      </SafeAreaProvider>
-    </QueryClientProvider>
+    <AuthCredentialsProvider>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <ThemeProvider theme={theme}>
+            <Router />
+            <Toast />
+          </ThemeProvider>
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </AuthCredentialsProvider>
   );
 }
 
--- src/services/authCredentials/useAuthCredentials.ts
+++ src/services/authCredentials/useAuthCredentials.ts
@@ -1,14 +1,29 @@
-import {create} from 'zustand';
+import {useContext} from 'react';
 
 import {AuthCredentialsService} from './authCredentialsTypes';
+import {AuthCredentialsContext} from './Providers/AuthCredentialsProvider';
 
 export function useAuthCredentials(): AuthCredentialsService {
-  return useAuthCredentialsZustand();
+  const context = useContext(AuthCredentialsContext);
+  if (!context) {
+    throw new Error(
+      'AuthCredentials should be used within a AuthCredentialsProvider',
+    );
+  }
+  return context;
 }
 
-const useAuthCredentialsZustand = create<AuthCredentialsService>(set => ({
-  authCredentials: null,
-  isLoading: false,
-  saveCredentials: async ac => set({authCredentials: ac}),
-  removeCredentials: async () => set({authCredentials: null}),
-}));
+// const useAuthCredentialsZustand = create<AuthCredentialsService>()(
+//   persist(
+//     set => ({
+//       authCredentials: null,
+//       isLoading: false,
+//       saveCredentials: async ac => set({authCredentials: ac}),
+//       removeCredentials: async () => set({authCredentials: null}),
+//     }),
+//     {
+//       name: '@Auth',
+//       storage: storage,
+//     },
+//   ),
+// );
