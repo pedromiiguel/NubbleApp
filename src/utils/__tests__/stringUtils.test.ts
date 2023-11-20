21fcff1533422bdfd752256713a18291ff1b24d1
Pedro Miguel <pedromrap@gmail.com>
2023-11-20 11:40:21 -0300

    test: add string utils tests

---
+++ src/utils/__tests__/stringUtils.test.ts
@@ -0,0 +1,21 @@
+import {stringUtils} from '../stringsUtils';
+
+describe('stringUtils', () => {
+  describe('capitalizeFirstLetter', () => {
+    it('should capitalize the first letter of each word', () => {
+      expect(stringUtils.capitalizeFirstLetter('Ana maria')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('ANA MARIA')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('ana maria')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('maria')).toBe('Maria');
+    });
+
+    it('should remove leading/trailing spaces', () => {
+      expect(stringUtils.capitalizeFirstLetter('  ANA MARIA')).toBe(
+        'Ana Maria',
+      );
+      expect(stringUtils.capitalizeFirstLetter('ana maria   ')).toBe(
+        'Ana Maria',
+      );
+    });
+  });
+});
