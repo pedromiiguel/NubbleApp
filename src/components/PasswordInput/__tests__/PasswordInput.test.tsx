d6199aecf577f00ad04bac01ad3de61eafa6f8da
Pedro Miguel <pedromrap@gmail.com>
2023-11-27 12:25:56 -0300

    test: add password input tests

---
+++ src/components/PasswordInput/__tests__/PasswordInput.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import {IconProps} from 'src/components/Icon/Icon';
+import {fireEvent, render, screen} from 'test-utils';
+
+import {PasswordInput} from '../PasswordInput';
+
+describe('<PasswordInput/>', () => {
+  it('starts with hidden password', () => {
+    const mockedOnChange = jest.fn();
+    render(
+      <PasswordInput
+        label="Password"
+        value="123456"
+        placeholder="password"
+        onChangeText={mockedOnChange}
+      />,
+    );
+
+    const inputElement = screen.getByPlaceholderText('password');
+
+    expect(inputElement.props.secureTextEntry).toBeTruthy();
+  });
+
+  it('when pressing the eye icon, it should show the password, and change to the eye off icon', () => {
+    const mockedOnChange = jest.fn();
+
+    render(
+      <PasswordInput
+        label="Password"
+        value="123456"
+        placeholder="password"
+        onChangeText={mockedOnChange}
+      />,
+    );
+
+    const eyeIcon: IconProps['name'] = 'eyeOn';
+
+    fireEvent.press(screen.getByTestId(eyeIcon));
+
+    const eyeOffIcon: IconProps['name'] = 'eyeOff';
+    const eyeOffIconElement = screen.getByTestId(eyeOffIcon);
+
+    expect(eyeOffIconElement).toBeTruthy();
+
+    const inputElement = screen.getByPlaceholderText('password');
+
+    expect(inputElement.props.secureTextEntry).toBeFalsy();
+  });
+});
