c8954a36977a688ec2df63cf73bf72f0c83d1c9f
Pedro Miguel <pedromrap@gmail.com>
2023-12-04 14:41:11 -0300

    chore: new alias

--- src/domain/PostComment/index.ts
+++ src/domain/PostComment/index.ts
@@ -3,3 +3,4 @@ export * from './postCommentService';
 export * from './postCommentTypes';
 export * from './useCases/usePostCommentCreate';
 export * from './useCases/usePostCommentRemove';
+export {POST_COMMENT_PATH} from './postCommentApi';
