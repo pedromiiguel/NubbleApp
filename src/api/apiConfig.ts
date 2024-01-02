dd568d5501d636067724c7a1b04ad1a3f3872246
Pedro Miguel <pedromrap@gmail.com>
2024-01-02 16:36:09 -0300

    feat: add register interceptor function

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,6 +1,59 @@
+import {AuthCredentials, authService} from '@domain';
 import axios from 'axios';
 
 export const BASE_URL = 'http://127.0.0.1:3333/';
+
 export const api = axios.create({
   baseURL: BASE_URL,
 });
+
+type InterceptorProps = {
+  authCredentials: AuthCredentials | null;
+  saveCredentials: (ac: AuthCredentials) => Promise<void>;
+  removeCredentials: () => Promise<void>;
+};
+
+export function registerInterceptor({
+  authCredentials,
+  removeCredentials,
+  saveCredentials,
+}: InterceptorProps) {
+  const interceptor = api.interceptors.response.use(
+    response => response,
+    async responseError => {
+      const failedRequest = responseError.config;
+      const hasNotRefreshToken = !authCredentials?.refreshToken;
+      const isRefreshTokenRequest =
+        authService.isRefreshTokenRequest(failedRequest);
+
+      console.log({
+        status: responseError.response.status,
+        url: responseError.url,
+        response: responseError.response,
+      });
+
+      if (responseError.response.status === 401) {
+        if (hasNotRefreshToken || isRefreshTokenRequest || failedRequest.sent) {
+          removeCredentials();
+          return Promise.reject(responseError);
+        }
+
+        failedRequest.sent = true;
+
+        const newAuthCredentials = await authService.authenticateByRefreshToken(
+          authCredentials?.refreshToken,
+        );
+        saveCredentials(newAuthCredentials);
+
+        failedRequest.headers.Authorization = `Bearer ${newAuthCredentials.token}`;
+
+        return api(failedRequest);
+      }
+
+      return Promise.reject(responseError);
+    },
+  );
+
+  // remove listener when component unmount
+  return () => api.interceptors.response.eject(interceptor);
+}
