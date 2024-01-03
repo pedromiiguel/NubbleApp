a157a2741be22e4ef71defd965194d42f46847c5
Pedro Miguel <pedromrap@gmail.com>
2024-01-03 15:40:00 -0300

    feat: add use user seach hook

--- src/infra/infraTypes.ts
+++ src/infra/infraTypes.ts
@@ -1,6 +1,7 @@
 export enum QueryKeys {
   UserGetById = 'UserGetById',
   PostList = 'PostList',
+  UserList = 'UserList',
   PostCommentList = 'PostCommentList',
   IsUserNameAvailable = 'IsUserNameAvailable',
 }
