9205cb1512250844a6a26c7b346c881ed43c2fa0
Pedro Miguel <pedromrap@gmail.com>
2024-01-22 09:46:47 -0300

    chore: export user path

--- src/domain/User/index.ts
+++ src/domain/User/index.ts
@@ -3,3 +3,4 @@ export * from './userService';
 export * from './userAdapter';
 export * from './useCases/useUserGetById';
 export * from './useCases/useUserSearch';
+export {USER_PATH} from './userApi';
