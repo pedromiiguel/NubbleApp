a2eff04b358e535a8eeeef2d5d17fd75e0865e38
Pedro Miguel <pedromrap@gmail.com>
2024-01-22 10:04:47 -0300

    test: add user handlers mock

--- src/test/server/server.ts
+++ src/test/server/server.ts
@@ -1,9 +1,11 @@
 import {setupServer} from 'msw/node';
 
 import {postCommentHandlers} from './PostComment/postCommentHandlers';
+import {userHandlers} from './User/userHandlers';
 
-export const server = setupServer(...postCommentHandlers);
+export const server = setupServer(...postCommentHandlers, ...userHandlers);
 
 export {mockedData as mockedPostComment} from './PostComment/mocks';
+export {userMocked} from './User/userMocked';
 
 export {resetInMemoryResponse} from './PostComment/postCommentHandlers';
