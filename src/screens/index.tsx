3ffc92a8db6409b1233840e2b1286bcd0d934c39
Pedro Miguel <pedromrap@gmail.com>
2024-02-19 17:44:42 -0300

    feat: add publish post screen

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -13,3 +13,5 @@ export * from './app/ProfileScreen/ProfileScreen';
 export * from './app/FavoriteScreen/FavoriteScreen';
 export * from './app/MyProfileScreen/MyProfileScreen';
 export * from './app/NewPostScreen/NewPostScreen';
+export * from './app/PublishPostScreen/PublishPostScreen';
+export * from './app/CameraScreen/CameraScreen';
