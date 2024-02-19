b12c64116f4d690b17adcf9b06c95940c1cf1df3
Pedro Miguel <pedromrap@gmail.com>
2024-02-19 17:35:42 -0300

    feat: finish new post screen

--- src/screens/app/NewPostScreen/NewPostScreen.tsx
+++ src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  ListRenderItemInfo,
+  Pressable,
+} from 'react-native';
 
-import {Text} from '@components';
+import {useCameraRoll, usePermission} from '@services';
+
+import {PermissionManager, Screen} from '@components';
 import {AppTabScreenProps} from '@routes';
+
+import {Header} from './components/Header';
+
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const NUM_COLUMNS = 4;
+const ITEM_WIDTH = SCREEN_WIDTH / NUM_COLUMNS;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function NewPostScreen(props: AppTabScreenProps<'NewPostScreen'>) {
-  return <Text preset="headingSmall">New Post Screen</Text>;
+  const [selectedImage, setSelectedImage] = useState<string>();
+  const permission = usePermission('photoLibrary');
+  const hasPermission = permission.status === 'granted';
+  const {photoList, fetchNextPage} = useCameraRoll(
+    hasPermission,
+    setSelectedImage,
+  );
+
+  const flatListRef = useRef<FlatList>(null);
+
+  function onSelectImage(imageUri: string) {
+    setSelectedImage(imageUri);
+    flatListRef.current?.scrollToOffset({offset: 0, animated: true});
+  }
+
+  function renderItem({item}: ListRenderItemInfo<string>) {
+    return (
+      <Pressable onPress={() => onSelectImage(item)}>
+        <Image
+          key={item}
+          source={{uri: item}}
+          style={{width: ITEM_WIDTH, height: ITEM_WIDTH}}
+          onError={error => console.error('Erro ao carregar imagem', error)}
+        />
+      </Pressable>
+    );
+  }
+
+  return (
+    <PermissionManager
+      permissionName="photoLibrary"
+      description="Permita o Nubble acessar as imagens da sua galeria">
+      <Screen canGoBack noPaddingHorizontal title="Novo post">
+        <FlatList
+          ref={flatListRef}
+          data={photoList}
+          numColumns={NUM_COLUMNS}
+          onEndReached={fetchNextPage}
+          onEndReachedThreshold={0.1}
+          renderItem={renderItem}
+          ListHeaderComponent={
+            <Header imageWidth={SCREEN_WIDTH} imageUri={selectedImage} />
+          }
+        />
+      </Screen>
+    </PermissionManager>
+  );
 }
