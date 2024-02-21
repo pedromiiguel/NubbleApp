67ac7b5010e2e3c7ef8f807a41ad273135a9480d
Pedro Miguel <pedromrap@gmail.com>
2024-02-21 18:01:41 -0300

    feat: add use post create hook

---
+++ src/domain/Post/useCases/usePostCreate.ts
@@ -0,0 +1,49 @@
+import {MutationOptions, QueryKeys} from '@infra';
+import {ImageForUpload, multimidiaService} from '@services';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
+
+import {postService} from '../postService';
+import {Post} from '../postTypes';
+
+export function usePostCreate(options?: MutationOptions<Post>) {
+  const queryClient = useQueryClient();
+
+  const {mutate, isLoading, isError} = useMutation<
+    Post,
+    unknown,
+    {name: string; imageCover: ImageForUpload}
+  >({
+    mutationFn: ({name, imageCover}) =>
+      postService.createPost(name, imageCover),
+    onSuccess: post => {
+      queryClient.invalidateQueries({queryKey: [QueryKeys.PostList]});
+
+      if (options?.onSuccess) {
+        options.onSuccess(post);
+      }
+    },
+    onError: () => {
+      if (options?.onError) {
+        options.onError(options.errorMessage || 'erro ao criar post');
+      }
+    },
+  });
+
+  function createPost({
+    description,
+    imageUri,
+  }: {
+    description: string;
+    imageUri: string;
+  }) {
+    const imageForUpload = multimidiaService.prepareImageForUpload(imageUri);
+
+    mutate({name: description, imageCover: imageForUpload});
+  }
+
+  return {
+    createPost,
+    isLoading,
+    isError,
+  };
+}
