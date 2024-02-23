e290c57c3f504b21dc9264f9fb597afd6b1aa293
Pedro Miguel <pedromrap@gmail.com>
2024-02-22 21:31:58 -0300

    feat: integration with usePostCreate hook

--- src/screens/app/PublishPostScreen/PublishPostScreen.tsx
+++ src/screens/app/PublishPostScreen/PublishPostScreen.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import {Dimensions, Image} from 'react-native';
 
+import {usePostCreate} from '@domain';
+import {useToastService} from '@services';
+
 import {Button, Screen, Text, TextInput} from '@components';
 import {AppScreenProps} from '@routes';
 
@@ -8,8 +11,26 @@ const IMAGE_WIDTH = Dimensions.get('screen').width / 2;
 
 export function PublishPostScreen({
   route,
+  navigation,
 }: AppScreenProps<'PublishPostScreen'>) {
   const [description, setDescription] = useState('');
+  const imageUri = route.params.imageUri;
+
+  const {showToast} = useToastService();
+
+  const {createPost, isLoading} = usePostCreate({
+    onSuccess: () => {
+      navigation.navigate('AppTabNavigator', {
+        screen: 'HomeScreen',
+      });
+
+      showToast({message: 'Foto publicada!', type: 'success'});
+    },
+  });
+
+  function publishPost() {
+    createPost({description, imageUri});
+  }
 
   return (
     <Screen scrollable canGoBack title="Novo Post">
@@ -34,7 +55,13 @@ export function PublishPostScreen({
         placeholder="Digite aqui..."
         containerProps={{borderWidth: 0}}
       />
-      <Button mt="s56" title="Publicar post" />
+      <Button
+        mt="s56"
+        title="Publicar post"
+        onPress={publishPost}
+        loading={isLoading}
+        disabled={description.length < 1}
+      />
     </Screen>
   );
 }
