ce50b88e33abfdca44d3dad566fd612131508fd1
Pedro Miguel <pedromrap@gmail.com>
2024-04-01 15:28:51 -0300

    feat: add use app color and use app color scheme

--- App.tsx
+++ App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
+import {settingsService, useAppColor} from '@services';
 import {ThemeProvider} from '@shopify/restyle';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Toast} from '@components';
+import {useAppColorScheme} from '@hooks';
+import {darkTheme, theme} from '@theme';
 
 import {Router} from './src/routes/routes';
 import {AuthCredentialsProvider} from './src/services/authCredentials/Providers/AuthCredentialsProvider';
 import {MMKVStorage, initializeStorage} from './src/services/storage';
-import {theme} from './src/theme/theme';
 
 initializeStorage(MMKVStorage);
 
 const queryClient = new QueryClient();
 
 function App(): JSX.Element {
+  useAppColorScheme();
+  const appColor = useAppColor();
+
+  useEffect(() => {
+    settingsService.handleStatusBar(appColor);
+  }, [appColor]);
+
   return (
     <AuthCredentialsProvider>
       <QueryClientProvider client={queryClient}>
         <SafeAreaProvider>
-          <ThemeProvider theme={theme}>
+          <ThemeProvider theme={appColor === 'dark' ? darkTheme : theme}>
             <Router />
             <Toast />
           </ThemeProvider>
