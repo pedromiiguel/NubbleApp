b196f420906858eebd822eebab5024a1d75316fb
Pedro Miguel <pedromrap@gmail.com>
2024-04-01 15:14:20 -0300

    feat: add content on setting screen

--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,20 +1,50 @@
 import React from 'react';
+import {FlatList, ListRenderItemInfo} from 'react-native';
 
 import {useAuthSignOut} from '@domain';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
-import {Button, Screen} from '@components';
+import {Button, Screen, Separator} from '@components';
 import {AppStackParamList} from '@routes';
+
+import {MenuItem, MenuItemProps} from './components/MenuItem';
 
 type ScreenProps = NativeStackScreenProps<AppStackParamList, 'SettingsScreen'>;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function SettingsScreen(props: ScreenProps) {
+export function SettingsScreen({navigation}: ScreenProps) {
   const {isLoading, signOut} = useAuthSignOut();
 
+  const items: MenuItemProps[] = [
+    {label: 'Termos de uso', onPress: () => {}},
+    {label: 'Política de privacidade', onPress: () => {}},
+    {
+      label: 'Modo escuro',
+      onPress: () => {
+        navigation.navigate('DarkModeScreen');
+      },
+    },
+  ];
+
+  function renderItem({item}: ListRenderItemInfo<MenuItemProps>) {
+    return <MenuItem {...item} />;
+  }
+
   return (
-    <Screen canGoBack title="Configurações">
-      <Button loading={isLoading} title="Sair da conta" onPress={signOut} />
+    <Screen canGoBack title="Configurações" flex={1}>
+      <FlatList
+        data={items}
+        bounces={false}
+        renderItem={renderItem}
+        ItemSeparatorComponent={Separator}
+        ListFooterComponent={
+          <Button
+            loading={isLoading}
+            mt="s48"
+            title="Sair da conta"
+            onPress={signOut}
+          />
+        }
+      />
     </Screen>
   );
 }
