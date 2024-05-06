9c01c54a55598c24bc9713390fb1a1fb9f0da6a5
Pedro Miguel <pedromrap@gmail.com>
2024-05-06 13:09:02 -0300

    chore: update others files for update react native version

--- src/services/permission/permissionService.android.ts
+++ src/services/permission/permissionService.android.ts
@@ -29,9 +29,14 @@ async function request(name: PermissionName): Promise<PermissionStatus> {
 }
 
 function mapNameToPermission(name: PermissionName): Permission | null {
+  /**
+   * Version is always a number in Android
+   * https://reactnative.dev/docs/platform#version
+   */
+  const platform = Platform.Version as number;
   switch (name) {
     case 'photoLibrary':
-      if (typeof Platform.Version === 'number' && Platform.Version >= 33) {
+      if (platform >= 33) {
         return 'android.permission.READ_MEDIA_IMAGES';
       } else {
         return 'android.permission.READ_EXTERNAL_STORAGE';
