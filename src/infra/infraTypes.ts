3dcd4414cad04c918cd63c10bb5d7ea9e77c8df9
Pedro Miguel <pedromrap@gmail.com>
2024-05-20 09:48:02 -0300

    feat: add post get by id and favorite list keys

--- src/infra/infraTypes.ts
+++ src/infra/infraTypes.ts
@@ -1,6 +1,8 @@
 export enum QueryKeys {
   UserGetById = 'UserGetById',
+  PostGetById = 'PostGetById',
   PostList = 'PostList',
+  FavoriteList = 'FavoriteList',
   UserList = 'UserList',
   PostCommentList = 'PostCommentList',
   IsUserNameAvailable = 'IsUserNameAvailable',
