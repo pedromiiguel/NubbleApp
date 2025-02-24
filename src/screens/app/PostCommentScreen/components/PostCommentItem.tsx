dbdc633047328d495fb9b49a13e588eb2d6b42b6
Pedro Miguel <pedromrap@gmail.com>
2025-02-24 09:17:50 -0300

    feat: add authorId prop to ProfileAvatar in PostCommentItem for improved user identification

--- src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
+++ src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
@@ -58,7 +58,10 @@ export function PostCommentItem({
         alignItems="center"
         mb="s16"
         paddingHorizontal="s24">
-        <ProfileAvatar imageURL={postComment.author.profileURL} />
+        <ProfileAvatar
+          imageURL={postComment.author.profileURL}
+          authorId={postComment.author.id}
+        />
         <Box ml="s12" flex={1}>
           <Text preset="paragraphSmall" bold>
             {postComment.author.userName}
