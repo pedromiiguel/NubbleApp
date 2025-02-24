ec9a84a89f28b8ae7d1b77f8269ca9967c80cb45
Pedro Miguel <pedromrap@gmail.com>
2025-02-24 09:16:05 -0300

    feat: refactor PostActions to use useAppNavigation for improved navigation

--- src/components/PostItem/components/PostActions.tsx
+++ src/components/PostItem/components/PostActions.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import {Post, useReactToPost} from '@domain';
 import {QueryKeys} from '@infra';
-import {useNavigation} from '@react-navigation/native';
 
 import {Box, TouchableOpacityBox, Icon, Text, IconProps} from '@components';
+import {useAppNavigation} from '@hooks';
 
 type Props = {
   post: Post;
@@ -12,7 +12,8 @@ type Props = {
 };
 
 export function PostActions({post, hideCommentAction}: Props) {
-  const navigation = useNavigation();
+  const navigate = useAppNavigation();
+
   const likeReaction = useReactToPost({post, postReactionType: 'like'});
   const favoriteReaction = useReactToPost({
     post,
@@ -21,10 +22,7 @@ export function PostActions({post, hideCommentAction}: Props) {
   });
 
   function navigateToComments() {
-    navigation.navigate('PostCommentScreen', {
-      postId: post.id,
-      postAuthorId: post.author.id,
-    });
+    navigate.toPostComment({postId: post.id, postAuthorId: post.author.id});
   }
 
   return (
