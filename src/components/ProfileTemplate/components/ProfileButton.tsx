843c65062880507193e814a5ffb321ed37be214e
Pedro Miguel <pedromrap@gmail.com>
2025-02-24 09:35:10 -0300

    feat: add navigation to EditProfileScreen in ProfileButton component

--- src/components/ProfileTemplate/components/ProfileButton.tsx
+++ src/components/ProfileTemplate/components/ProfileButton.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {useNavigation} from '@react-navigation/native';
+
 import {Button, ButtonProps} from '../../Button/Button';
 
 type ButtonVariant = 'myProfile' | 'isFollowing' | 'isNotFollowing';
@@ -28,10 +30,20 @@ type ProfileButtonProps = {
 };
 
 export function ProfileButton({isFollowing, isMyProfile}: ProfileButtonProps) {
+  const navigation = useNavigation();
+
   const variant = getVariant({isFollowing, isMyProfile});
   const buttonProps = buttonVariants[variant];
 
-  return <Button {...buttonProps} marginVertical="s24" />;
+  function handleOnPress() {
+    if (isMyProfile) {
+      navigation.navigate('EditProfileScreen');
+    }
+  }
+
+  return (
+    <Button {...buttonProps} onPress={handleOnPress} marginVertical="s24" />
+  );
 }
 
 function getVariant({
